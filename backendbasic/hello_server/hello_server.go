6dad0b944c7c20dcba56f47db5cb4bcb261f4d87
Vojan Najov <serejah.mashichev@gmail.com>
2024-03-06 00:03:17 +0300

    Add hello server sample with handlerFunc

--- backendbasic/hello_server/hello_server.go
+++ backendbasic/hello_server/hello_server.go
@@ -33,3 +33,30 @@ var _ http.Handler = TextHandler("") // ensure TextHandler implements http.Handl
 func (t TextHandler) ServeHTTP(w http.ResponseWriter, _ *http.Request) {
 	w.Write([]byte(t)) // implicit 200 OK
 }
+
+// The http.Handler interface only has a single method, ServeHTTP, so it seems like
+// overkill to have to define a new type for every handler, when you could just use
+// a function. Use the http.HandlerFunc type to turn a function of type
+// func(http.ResponseWriter, *http.Request) into an http.Handler.
+
+// The following complete program is identical to the previous one, but uses
+// http.HandlerFunc instead of a custom type.
+
+/*
+func helloWorld(w http.ResponseWriter, r *http.Request) {
+  w.Write([]byte("hello, world!\r\n"))
+}
+
+func main() {
+  server := http.Server{Addr: ":8080", Handler: http.HandlerFunc(helloWorld)}
+  go server.ListenAndServe()
+  req, _ := http.NewRequestWithContext(
+    context.TODO(), "GET", "http://localhost:8080", nil,
+  )
+  resp, err := new(http.Client).Do(req)
+  _ = err
+  defer resp.Body.Close()
+  resp.Write(os.Stdout) // print the response to stdout
+}
+
+*/

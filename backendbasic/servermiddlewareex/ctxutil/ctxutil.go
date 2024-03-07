d0ba36a9b8b2278dea7f7a55a5db20e0126138ac
Vojan Najov <serejah.mashichev@gmail.com>
2024-03-08 02:12:21 +0300

    Add server middleware sample

---
+++ backendbasic/servermiddlewareex/ctxutil/ctxutil.go
@@ -0,0 +1,20 @@
+package ctxutil
+
+import "context"
+
+// key is a unique type that we can use as a key in a context
+type key[T any] struct{}
+
+// WithValue returns a new context with the given value set. Only one value of each
+// type can be set in context; setting a value of the same type will overwrite the
+// the previous value.
+func WithValue[T any](ctx context.Context, value T) context.Context {
+	return context.WithValue(ctx, key[T]{}, value)
+}
+
+// Value returns the value of type T in the given context, or false if the context
+// does not contain a value of type T.
+func Value[T any](ctx context.Context) (T, bool) {
+	value, ok := ctx.Value(key[T]{}).(T)
+	return value, ok
+}
---
+++ backendbasic/servermiddlewareex/go.mod
@@ -0,0 +1,5 @@
+module github.com/Vojan-Najov/exercises_go/backendbasic/servermiddlewareex
+
+go 1.21.6
+
+require github.com/google/uuid v1.6.0 // indirect
